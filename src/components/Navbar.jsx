8c6f55bf37f1acc6ddcab1712d1bbf3d5c1b4ad5
okkyprasetian <okkyprasetian@gmail.com>
2021-12-02 07:36:46 +0700

    add data.js and import it to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { dataLinks, dataSocials } from 'data'
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="nav-header">
+                <h1>Okky Prasetia</h1>
+                <h3>Burger</h3>
+            </div>
+            <div className="nav-links show-container">
+                <ul className="links">
+                </ul>
+            </div>
+            <ul className="nav-social">
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { FaGithub, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+export const dataLinks = [
+    {
+        id: 1,
+        url: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        url: '/',
+        name: 'About'
+    },
+    {
+        id: 3,
+        url: '/',
+        name: 'Contact'
+    },
+    {
+        id: 4,
+        url: '/',
+        name: 'Products'
+    }
+]
+
+export const dataSocials = [
+    {
+      id: 1,
+      url: 'https://www.facebook.com',
+      icon: <FaFacebook />,
+    },
+    {
+      id: 2,
+      url: 'https://www.twitter.com',
+      icon: <FaTwitter />,
+    },
+    {
+      id: 3,
+      url: 'https://www.linkedin.com',
+      icon: <FaLinkedin />,
+    },
+    {
+      id: 4,
+      url: 'https://www.github.com',
+      icon: <FaGithub />,
+    },
+  ]
+  
